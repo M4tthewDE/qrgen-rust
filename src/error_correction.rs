f55a3e22777a0093af0a84d57d9e131696773cbf
M4tthewDE <mattider2@gmail.com>
2021-06-30 20:25:20 +0200

    create lookup tables for multiplication with logarithms

--- src/error_correction.rs
+++ src/error_correction.rs
@@ -1,3 +1,5 @@
+const PRIM: u32 = 0x11d;
+
 // for version 1 and error correction L
 pub fn add_error_correction(data: Vec<String>) {
     // Total number of codewords: 26
@@ -5,12 +7,12 @@ pub fn add_error_correction(data: Vec<String>) {
     // Number of error correction blocks: 1
     // Error correction code per block: (26,19,2)
 
-    println!("{}", format!("{:b}", gf_mult_no_lut(0b10001001, 0b00101010, 0x11d)));
+    init_tables()
 }
 
-fn gf_mult_no_lut(x: i32, y:i32, prim: i32) -> i32 {
+fn gf_mult_no_lut(x: u32, y:u32, prim: u32) -> u32 {
 
-    fn cl_mult(x: i32, y: i32) -> i32 {
+    fn cl_mult(x: u32, y: u32) -> u32 {
         let mut z = 0;
         let mut i = 0;
         while (y >> i) > 0 {
@@ -23,7 +25,7 @@ fn gf_mult_no_lut(x: i32, y:i32, prim: i32) -> i32 {
     }
 
     // compute position of the most significant bit
-    fn bit_length(n: i32) -> u32 {
+    fn bit_length(n: u32) -> u32 {
         let mut bits = 0;
         for i in 0..(n.count_ones()+n.count_zeros()) {
             if (n >> i & 1) == 1 {
@@ -35,7 +37,7 @@ fn gf_mult_no_lut(x: i32, y:i32, prim: i32) -> i32 {
     }
 
     // carry-less long division, returns remainder
-    fn cl_div(mut dividend: i32, divisor: i32) -> i32 {
+    fn cl_div(mut dividend: u32, divisor: u32) -> u32 {
         let dl1 = bit_length(dividend);
         let dl2 = bit_length(divisor);
 
@@ -58,4 +60,25 @@ fn gf_mult_no_lut(x: i32, y:i32, prim: i32) -> i32 {
         result = cl_div(result, prim);
     }
     result
+}
+
+fn init_tables() {
+    let mut gf_exp: Vec<u32> = (0..512).collect();
+    let mut gf_log: Vec<u32> = (0..256).collect();
+
+    let mut count_helper = 0;
+    let mut x = 1;
+    for i in gf_exp.iter_mut() {
+        *i = x;
+        if count_helper < 255 {
+            gf_log[x as usize] = count_helper;
+            count_helper += 1;
+        }
+        x = gf_mult_no_lut(x, 2, PRIM);
+    }
+
+    for i in 255..512 {
+        gf_exp[i] = gf_exp[i-255]
+    }
+    println!("{:?}", gf_log);
 }
